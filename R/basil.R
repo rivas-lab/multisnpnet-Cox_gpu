ce605c0d6987b2fb4813a1b165783c02f5f13e31
RuilinLi <ruilinli@stanford.edu>
2020-09-25 22:01:37 +0000

    control max number of variables per iteration

--- R/basil.R
+++ R/basil.R
@@ -3,10 +3,10 @@
 basil = function(genotype.pfile, phe.file, responsid, covs = NULL, 
                  nlambda = 100, lambda.min.ratio = 0.01, 
                  alpha=NULL, p.factor = NULL,configs = NULL,
-                 num_lambda_per_iter = 10, num_to_add = 200)
+                 num_lambda_per_iter = 10, num_to_add = 200, max_num_to_add = 6000)
 {
   basil_base(genotype.pfile, phe.file, responsid, covs, 
                       nlambda, lambda.min.ratio, 
                       alpha, p.factor,configs,
-                      num_lambda_per_iter, num_to_add,  solve_aligned_gpu)
+                      num_lambda_per_iter, num_to_add, max_num_to_add, solve_aligned_gpu)
 }
